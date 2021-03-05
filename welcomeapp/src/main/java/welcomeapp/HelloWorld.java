e616085df585ba80c227d1be0bb6a8a6896ee41d
javeed512 <javeedmohammedjaveed@gmail.com>
2021-03-05 11:48:53 +0530

    add gradle project

---
+++ welcomeapp/src/main/java/welcomeapp/HelloWorld.java
@@ -0,0 +1,22 @@
+package welcomeapp;
+
+public class HelloWorld {
+	
+	
+	 public static void main(String[] args) {
+			
+	    	
+	    	System.out.println("Welcome Friends...");
+	    	
+		}
+	    
+	    
+	    public static String getName() {
+	    	
+	    	
+	    	return "javeed";
+	    	
+	    }
+	    
+
+}
---
+++ welcomeapp/src/test/java/welcomeapp/HelloWorldTest.java
@@ -0,0 +1,19 @@
+package welcomeapp;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HelloWorldTest {
+
+	@Test
+	void testGetName() {
+
+
+		assertEquals("javeed", HelloWorld.getName());
+		
+		
+		
+	}
+
+}
